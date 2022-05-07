a7d54259cb0062f33cfa5bb54f71c2a9bfda4a1f
Oleksii Borodin <103507359+Guyfromforest@users.noreply.github.com>
2022-05-07 13:49:26 +0300

    Home work

---
+++ task2.py
@@ -0,0 +1,18 @@
+
+#without anything
+print ("Today is good weather")
+print ("today is good weather","dog is happy", sep="/")
+print("today is good weather\n", end="???")
+print("today is good wether\n","god is funy", sep = "///")
+
+print("######\n"
+      "#\t",  "#\n"
+      "#\t",  "#\n"
+      "#\t",  "#\n"
+      "######\n")
+
+print("#\t",  "#\n"
+      "#\t",  "#\n"
+        "######\n"
+      "#\t",  "#\n"
+      "#\t",  "#\n")
