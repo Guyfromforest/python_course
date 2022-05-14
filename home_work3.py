3cf739d9267add6cc10b91f243d4f18b1717e6e7
Oleksii Borodin <103507359+Guyfromforest@users.noreply.github.com>
2022-05-14 12:47:42 +0300

    Done home_work3

---
+++ home_work3.py
@@ -0,0 +1,19 @@
+#Make a program that has your name and the current day of the week stored as separate variables and then prints a message like this:
+name = str("Oleksii Borodin")
+day = str("Friday")
+print("Good day", name,"!",day, "is a perfect day to leand some python")
+#Save your first and last name as separate variables, then use string concatenation to add them together with a white space in between and print a greeting.
+a = str("Oleksii\t")
+s = str("Borodin")
+print(a + s)
+
+#Make a program with 2 numbers saved in separate variables a and b, then print the result for each of the following:
+a = int(2)
+b = int(4)
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a**b)
+print(a%b)
+print(a//b)
