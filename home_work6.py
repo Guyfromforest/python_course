ababf8660b5a993804def108b39ee965702bc41b
Oleksii Borodin <103507359+Guyfromforest@users.noreply.github.com>
2022-05-21 23:31:15 +0300

    Add files via upload

---
+++ home_work6.py
@@ -0,0 +1,32 @@
+#Write a Python program to get the largest number from a list of random numbers with the length of 10
+#Constraints: use only while loop and random module to generate numbersa =
+# import random
+# random
+# list1 = random.sample(range(1, 11), 10)
+# largest= max(list1)
+# print("Largest is:", largest)
+
+# Exclusive common numbers.
+
+#Generate 2 lists with the length of 10 with random integers from 1 to 10, and make a third list containing the common integers between the 2 initial lists without any duplicates.
+
+#Constraints: use only while loop and random module to generate numbers
+# import random
+# list_a= random.sample(range(1,11), 10)
+# list_b= random.sample(range(1, 11), 10)
+# list_c= []
+# for i in list_a and list_b:
+#     if i in list_a and list_b:
+#         list_c.append(i)
+# print(sorted(list_c))
+
+#Task 3
+
+# list_1 = [*range(1,101,1)]
+# list_2=[]
+# print(list_1)
+# for i in list_1:
+#     if i%7 == 0 and i%5!= 0:
+#         list_2.append(i)
+# print(list_2)
+
