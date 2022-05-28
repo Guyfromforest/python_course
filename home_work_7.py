bbe910dcd94f37600f7920421d9b6625aa2fe588
Oleksii Borodin <103507359+Guyfromforest@users.noreply.github.com>
2022-05-28 21:33:34 +0300

    Add files via upload

---
+++ home_work_7.py
@@ -0,0 +1,25 @@
+#Make a program that has some sentence (a string) on input and returns a dict containing all unique words as keys and the number of occurrences as values.
+#dict1={}
+# s = input(str("How are you today?\n"))
+# s1 = s.split()
+# s2 = {s1[i]: s1[i+1] for i in range(0, len(s1),2)}
+# print(s2)
+# print(type(s2))
+ 
+ 
+#2Create a function which takes as input two dicts with structure mentioned above, then computes and returns the total price of stock. stock = {
+# stock = {'banana': 6, 'apple': 0, 'orange': 32, 'pear': 15}
+# prices = {'banana': 4, 'apple': 2, 'orange': 1.5, 'pear': 3}
+# stocks = {v: stock[v]*prices[v] for v in prices}
+# total = sum(stocks.values())
+# print("The stocks are:\n", stocks)
+# print("The total amount of goods are:", (total))
+
+#Use a list comprehension to make a list containing tuples (i, j) where `i` goes from 1 to 10 and `j` is corresponding to `i` squared.
+
+# i = tuple([x for x in range(11)])
+# j = tuple([x**2 for x in range(11)])
+# tuple1 = i+j
+# print(tuple1)
+
+
